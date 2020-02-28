ba0dc10374cf6b0ce9fb034109a42e38cb8bdbec
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-28 14:29:47 +0000

    Removed Test

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-console.log("Test");
-
-
 const question = document.getElementsById("question");
-const choices = document.getElementsById("choice-text");
+const choices = Array.from(document.getElementsById("choice-text"));
+
+let currenQuestion = {};
+let acceptingAwnsers = true;
+let score = 0;
+let questionCounter = 0;
+let avalibleQuestions = [];
