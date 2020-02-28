e8887ace8665f23114e9e7f7d5fffc2b95567b23
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-28 17:06:07 +0000

    Modified Script

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ let acceptingAwnsers = true;
 let score = 0;
 let questionCounter = 0;
 let avalibleQuestions = [];
+
+let questions = [
+{
+  question: "who won the premier league in 2007?"
+  choice1: console.writeLine("Test");
+}
+
+
+
+];
