6cf2a177bb5679a3a5eae90ddcd4ebe1297571b3
Gulam Qasim <gfram@hotmail.co.uk>
2020-03-01 20:30:05 +0000

    Fixed Var

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 let currentQuestion = {};
-let acceptingAwnsers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
@@ -26,38 +26,37 @@ let questions = [
 }
 ];
 
-//Constants
-const Correct_Bounds = 10;
-const Max_Questions = 5;
+
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const Max_Questions = 3;
 
 startGame = () => {
   questionCounter = 0;
   score = 0;
-  //the 3 dots is taking our exitsting array and spreading
-  //them out and then putting them into a new array
   availableQuesions = [...questions];
   console.log(availableQuesions);
   getNewQuestion();
 };
 
 getNewQuestion = () => {
-  if(availableQuesions.length === 0 || questionCounter >= Max_Questions) {
+  if (availableQuesions.length === 0 || questionCounter >= Max_Questions) {
     //go to the end page
     return window.location.assign("/end.html");
   }
   questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
-    currentQuestion = availableQuesions[questionIndex];
-    question.innerText = currentQuestion.question;
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
 
   choices.forEach(choice => {
     const number = choice.dataset["number"];
-    choice.innerText = currentQuestion["choice" + number]
+    choice.innerText = currentQuestion["choice" + number];
   });
 
-  avaliableQuestions.splice(questionIndex, 1);
-  console.log(avaliableQuestions);
-  acceptingAwnsers = true;
+  availableQuesions.splice(questionIndex, 1);
+  console.log(availableQuesions);
+  acceptingAnswers = true;
 };
 
 choices.forEach(choice => {
